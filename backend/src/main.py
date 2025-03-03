73f0019f9faf198d2e669e407aadda259c968185
David Itcovici <daviditcovici0@gmail.com>
2025-03-03 19:59:04 -0300

    Add dependencies

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=['http://localhost:8080'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:5000';
 
 createApp(App).use(router).mount('#app')
